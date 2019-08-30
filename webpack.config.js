0f2d99edc29ccb3b6e9de75bc465b7ff1fde4893
Amy W <49384+keisisqrl@users.noreply.github.com>
2019-08-29 17:27:38 -0700

    webpack migration (from parcel)

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      use: {
+        loader: 'elm-webpack-loader',
+        options: {
+          debug: true
+        }
+      }
+    }
+  ]
+  }
+};
